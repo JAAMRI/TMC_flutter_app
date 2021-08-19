436ea8f586f708780ea97e4d3e1a0330a2687ad2
Jyhad Aamri <j.aamri@hotmail.com>
2021-08-18 20:22:40 -0400

    App icon and assets folders added

--- lib/screens/login/components/login_body.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class LoginBody extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
--- lib/screens/login/login_screen.dart
+++ lib/screens/login/login_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:tmc_flutter_app/screens/login/components/login_body.dart';
 
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: LoginBody(),
+        child: Column(
+          children: [
+            Image(
+              width: 50.0,
+              image: AssetImage('assets/images/theMuslimCompanionIcon.png'),
+            )
+          ],
+        ),
       ),
     );
   }
