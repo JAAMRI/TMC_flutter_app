5f141fe6fdaa8257d94554e90eb6f9c814c8d704
Jyhad Aamri <j.aamri@hotmail.com>
2021-08-21 19:27:01 -0400

    background color added

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF8acac0);
-const kPrimaryLightColor = Color(0xFFfdce84);
+const kBackgroundColor = Color(0xFF202020);
+const kPrimaryColor = Color(0xFFFFBD73);
--- lib/main.dart
+++ lib/main.dart
@@ -13,12 +13,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Auth',
+      title: 'Auth screen',
       theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
+          primaryColor: kPrimaryColor,
+          scaffoldBackgroundColor: kBackgroundColor),
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 6,
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/bg_image.jpeg"),
+                ),
+              ),
+            ),
+          ),
+          Expanded(child: Column())
+        ],
       ),
-      home: Text("Coming now"),
     );
   }
 }
